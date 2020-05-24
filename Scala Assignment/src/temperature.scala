cd3c2928bdafbb69dbbdf06d2441d2d007adfb3b
ashiviraji <46102737+ashiviraji@users.noreply.github.com>
2020-05-24 08:45:46 +0530

    Temperature Quiz

---
+++ Scala Assignment/src/temperature.scala
@@ -0,0 +1,9 @@
+
+
+object temperature {
+  def main(args:Array[String]){
+    val temp:Double=35
+    val faranhite:Double=temp*1.80000+32.00
+    println("The temperature is:"+faranhite+"F")
+  }
+}
